32424378f77487ee711b33e3f3783b3ed9d84a1b
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-25 15:11:32 -0500

    commit tree 47086e6b0f777c41470299fb90c5388e6698b9ff

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+def talk
+puts "Hello World!"
+end
+
+def walk
+    puts "The Person is walking"
+end
+end
+
+
+# Person
+#   is defined
+#   #talk
+#     responds to the #talk instance method
+#     puts "Hello World!"
+#   #walk
+#     responds to the #walk instance method
+#     puts "The Person is walking"
+
+# Finished in 0.02308 seconds (files took 0.08755 seconds to load)
+# 10 examples, 0 failures
